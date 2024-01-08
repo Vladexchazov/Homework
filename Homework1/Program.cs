8c0c428fba551733e39c8872f57354217cf5b569
Vlad@ <chazovvlad17@mail.ru>
2024-01-08 22:52:55 +0300

    Домашнее задание 1.

---
+++ Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = 46;
+int num1 = 7;
+int num2 = 23;
+if(num%num1 == 0){
+    Console.WriteLine("Да, число кратно 7!");
+}
+else{
+    Console.WriteLine($"Нет число не кратно 7, {num%num1}");
+}
+if(num%num2 == 0){
+    Console.WriteLine("Да число кратно 23!");
+}
+else{
+    Console.WriteLine($"Нет, число не кратно 23, {num%num2}");
+}
