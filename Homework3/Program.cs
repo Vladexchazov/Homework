89f226a160591aae1e2715e2923586381b1d2b45
Vlad@ <chazovvlad17@mail.ru>
2024-01-09 22:59:06 +0300

    Домашнее задание 3

---
+++ Homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = 65;
+int num1 = num/10; 
+int num2 = num%10;
+if(num  > 10 && num < 99){
+if (num1 > num2){
+    
+    Console.Write($"{num1} больше, чем {num2}");
+}
+else  {
+   
+    Console.Write($"{num2} больше, чем {num1}");
+}
+}
+else {
+    Console.Write("Число не двузначное");
+}
+
