93ad41a4d9715477d049984f6f19de03bee3def0
Vlad@ <chazovvlad17@mail.ru>
2024-01-09 22:58:23 +0300

    Домашнее задание 4

---
+++ Homework4/Program.cs
@@ -0,0 +1,19 @@
+﻿int N = 1234;
+if (N < 10){
+   Console.WriteLine(N);
+}
+else{
+ while (N > 0){
+ int num = N % 10;
+  N /= 10;
+ if (N > 0){
+
+   Console.Write(num + ",");
+ }
+
+else{
+Console.WriteLine(num);
+}
+ }
+}
+
